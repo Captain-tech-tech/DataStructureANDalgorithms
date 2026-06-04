8247e326b6246ecf42c61319e9057ade5e17e3e4
Captain-tech-tech <mmatif1112@gmail.com>
2026-06-04 17:02:02 +0500

    Some theory concepts related to data strucure

---
+++ IntroductionDATAstructure.cpp
@@ -0,0 +1,82 @@
+// Data : Data is raw, unorganized facts that need to be processed. It can be simple and at first glance 
+// seems random and useless. It is the foundation of all computation and information processing.
+
+// Structure : A structure refers to the way data is organized and related. It defines the relationships 
+// between different pieces of data
+
+// Data Structure : Data structure is a way of organizing and storing data in a computer's main memory so 
+// that it can be used efficiently.
+
+// Program : A program is a set of instructions that tells a computer what to do. It manipulates data to achieve 
+// a specific task
+
+// Data and program : Program operates on data. The way data is organized(data structure) significantly 
+// impact the program's efficiency. 
+
+// Data structure in program : Choosing the right data structure is crucial for writing efficient and effective
+// programs. It makes the progams more readable, maintainable and scalable
+
+// Data is Hard Drive and data structure is in main memory
+
+// Organizing data allows us for faster searching,sorting and retrivel
+// It simplifies data management making it easier to update, insert and delete records
+// efficient data structure optimize storage space, reducing memory usage 
+
+
+// An algorithm is a step-by-step procedure or set of instructions used to solve a problem or perform a task.
+// An algorithm is a finite sequence of steps/instructions to solve a specific problem
+
+// Efficient algorithm helps in faster procssing and resource optimization
+
+
+
+
+// +++++++++++++flowchart++++++++++
+// start ---> written inside an elliptical circle 
+// now inputs and outputs are written inside parallelogram
+// conditions are written inside diamonds
+// exit is writen inside an elliptical circle 
+
+// writing all the above work in steps is pseudocode
+
+
+
+
+
+
+
+
+
+
+// Algorithm analysis is the process of determining the amount of resources (like time and storage) required by 
+// an algorithm to solve a problem. It involves evaluating the efficiency and performance of an algorithm in 
+// terms of its computational complexity
+
+// Algorithm analysis is used to evaluate the efficiency of an algorithm in terms of time and memory usage
+// so that the most suitable algorithm can be selected for a given problem.
+
+// Algorithm analysis is important to compare different algorithms.
+// Algorithm analysis helps us estimate how an algorithm will perform as the input size grows
+// It helps to save time, money and to build scalable software
+
+// Algorithm Analysis is the broader process of evaluating an algorithm.
+// Complexity Analysis is the specific part where we measure time and space complexity.
+// Complexity Analysis is the study of the time and space requirements ofan algorithm as a function of the input size.
+
+// linear search 
+// binary search works on sorted arrays  
+
+
+
+// ---------+++++++++++ TIME COMPLEXITY ++++++++++++--------
+// time complextiy measures the growth rate of an algorithm's execution time as the input size increases
+// it's different from the actual time because the latter depends on hardware, software and other factors
+
+
+
+
+
+
+
+
+
