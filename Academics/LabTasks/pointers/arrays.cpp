57f634749fb12ad94079db12c88eb81f10ee53a1
Captain-tech-tech <mmatif1112@gmail.com>
2026-08-22 13:24:22 +0500

    dynamically alocate memory for 2D array

---
+++ Academics/LabTasks/pointers/arrays.cpp
@@ -0,0 +1,36 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int r,c;cout<<"Enter the number of rows and columns of the 2D array : ";cin>>r>>c;
+
+//     int *arr = new int[r*c];
+
+//     for(int i=0;i<r;i++)
+//     {
+//         cout<<"Enter "<<c<<" elements in the "<<i+1<<" row : ";
+//         for(int j=0;j<c;j++)
+//         {
+//             cin>>*(arr + r*i + j);
+//         }
+//     }
+
+//     cout<<endl<<endl<<"****** YOur 2D array ******"<<endl;
+//     for(int i=0;i<r;i++)
+//     {
+//         for(int j=0;j<c;j++)
+//         {
+//             cout<<*(arr + r*i + j)<<"  ";
+//         }
+//         cout<<endl;
+//     }
+
+//     delete[] arr;
+//     return 0;
+// }
+
+
+
+
+
+
