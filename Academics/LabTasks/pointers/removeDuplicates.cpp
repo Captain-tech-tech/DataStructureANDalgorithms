f11bf92cc93b0a08bce74b03b60e55c13954d396
Captain-tech-tech <mmatif1112@gmail.com>
2026-08-22 11:37:47 +0500

    changing the remove duplcate program

--- Academics/LabTasks/pointers/removeDuplicates.cpp
+++ Academics/LabTasks/pointers/removeDuplicates.cpp
@@ -1,53 +1,42 @@
 #include<iostream>
 using namespace std;
 
-int remove_duplicates(int s, int * arr) { 
-    for(int i=0;i<s;i++) 
-    { 
-        for(int j=i+1;j<s;j++) 
-        { 
-            if(*(arr+i) == *(arr+j)) 
-            { 
-                for(int k = j; k < s - 1; k++) 
+int remove_duplicates(int *arr, int s)
+{
+    for(int i=0;i<s;i++)
+    {
+        for(int j=i+1;j<s;j++)
+        {
+            if(*(arr+i) == *(arr+j))
+            {
+                for(int k=j;k<s-1;k++)
                 {
                     *(arr+k) = *(arr+k+1);
                 }
-                s--;
                 j--;
-            } 
-        } 
-    } 
-    return s; 
+                s--;
+            }
+        }
+    }
+
+    return s;
 }
 
 int main()
 {
     int s;cout<<"Enter the size of the array : ";cin>>s;
-
     int arr[s];
-    cout<<"Enter "<<s<<" elements in your array : ";
-
+    cout<<"Enter "<<s<<" elements in the array : ";
     for(int i=0;i<s;i++)
     {
         cin>>arr[i];
     }
 
-    cout<<endl<<"Your array with duplicates : ";
-    for(int i=0;i<s;i++)
+    int n = remove_duplicates(arr,s);
+    cout<<"Your array without duplicates : ";
+    for(int i=0;i<n;i++)
     {
-        cout<<arr[i]<<" ";
+        cout<<*(arr+i)<<"  ";
     }
-
-    cout<<endl<<"Your array without duplicates : ";
-
-    int n = remove_duplicates(s,arr);
-
-    for(int i = 0;i<n;i++)
-    {
-        cout<<*(arr+i)<<" ";
-    }
-    return 0;
+    cout<<endl;
 }
-
-
-
